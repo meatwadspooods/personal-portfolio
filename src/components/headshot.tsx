ac03fb9cda06aae41608430ae6110ef19c8f24df
meatwadspooods <elhamdiadam@gmail.com>
2025-04-19 06:01:53 -0500

    moved the basestyles out of the headshot component so they arent recreated every single time the component is rendered

--- src/components/headshot.tsx
+++ src/components/headshot.tsx
@@ -6,12 +6,13 @@ type HeadshotProps = {
   className?: string;
 }
 
+//base styles
+const baseStyles = "rounded-full relative overflow-hidden mb-4"
+
 const Headshot: React.FC<HeadshotProps> = ({src, alt = "Adam Elhamdi headshot", className = "w-[150px] h-[150px]"}) => {
-  //base styles
-  const basestyles = "rounded-full relative overflow-hidden mb-4"
 
   return (
-    <div className={`${basestyles} ${className}`}>
+    <div className={`${baseStyles} ${className}`}>
       <img src={src} alt={alt} className='w-full h-full object-cover'/>
     </div>
   );
